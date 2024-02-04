5f2b58128c92a2e16c2d5b35c8d2f569e42ac813
thiloker <thilokawith@gmail.com>
2024-02-04 16:54:22 +1100

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,28 @@
+export const Questions =[
+{
+    prompt:"What is 1+3",
+    optionA: "24",
+    optionB:"21",
+    optionC:"4",
+    optionD:"2",
+    answer: "C"
+},
+{
+    prompt:"What is 5+5",
+    optionA: "24",
+    optionB:"10",
+    optionC:"4",
+    optionD:"2",
+    answer: "B"
+},
+{
+    prompt:"What is 20+20",
+    optionA: "21",
+    optionB:"40",
+    optionC:"4",
+    optionD:"2",
+    answer: "A"
+},
+
+
+]
